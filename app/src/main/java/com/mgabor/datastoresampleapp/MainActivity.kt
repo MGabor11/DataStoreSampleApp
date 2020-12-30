78e6b99a50e4de7132649dcb74cadb50800ebb2f
gabor.marosfalvi <gabor.marosfalvi@supercharge.io>
2020-12-30 20:39:42 +0100

    Initial commit

---
+++ app/src/main/java/com/mgabor/datastoresampleapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mgabor.datastoresampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mgabor.datastoresampleapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
